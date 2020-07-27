408c57fcd8223de17cf6e86fa7d098a45d851df9
Michael Matibag <michael_matibag@qbsol.com>
2020-07-27 10:46:17 -0500

    Add initial solution files.

---
+++ SugarRollsBakery.Web/Controllers/ScheduleController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SugarRollsBakery.Web.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
